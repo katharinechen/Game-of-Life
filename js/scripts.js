3bb6e56b411ed6e1ec35f508e3f2c3fec468c5df
Katharine Chen & Justin Speers <student@epicodus.com>
2014-07-18 11:23:42 -0700

    Created ability to check status of cells and count neighbors

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,59 @@
-// Project Scripts
+
+
+var GameOfLife = {
+  initialize: function(sizeX, sizeY) {
+    this.world = [];
+    this.sizeX = sizeX;
+    this.sizeY = sizeY;
+    for(var index = 0; index < sizeX * sizeY; index++) {
+      this.world.push(false);
+    }
+    return this.world;
+  },
+  // returns the index (place to read/write) for this.world
+  // array
+  // returns -1 for invalid indexes
+  getIndex: function(x, y) {
+    if (x >= this.sizeX || x < 0 || y > this.sizeY || y < 0) {
+      return -1;
+    }
+    return y * this.sizeX + x;
+  },
+
+  setStatus: function(x, y, isAlive) {
+    var index = this.getIndex(x, y);
+    if (index === -1) { return -1; }
+    this.world[this.getIndex(x, y)] = isAlive;
+    return 1;
+  },
+
+  getStatus: function(x, y) {
+    var index = this.getIndex(x, y);
+    if (index === -1) { return false; }
+    return this.world[this.getIndex(x, y)];
+
+  },
+
+  countNeighbors: function(x,y) {
+    var count = 0;
+
+    // check the left neighbor
+    if (this.getStatus(x-1, y))   { count ++; }
+    if (this.getStatus(x+1, y))   { count ++; }
+    if (this.getStatus(x-1, y-1)) { count ++; }
+    if (this.getStatus(x,   y-1)) { count ++; }
+    if (this.getStatus(x+1, y-1)) { count ++; }
+    if (this.getStatus(x-1, y+1)) { count ++; }
+    if (this.getStatus(x,   y+1)) { count ++; }
+    if (this.getStatus(x+1, y+1)) { count ++; }
+
+    return count;
+  }
+
+};
+
+
+// Any live cell with fewer than two live neighbours dies, as if caused by under-population.
+// Any live cell with two or three live neighbours lives on to the next generation.
+// Any live cell with more than three live neighbours dies, as if by overcrowding.
+// Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
--- js/specs.js
+++ js/specs.js
@@ -1,1 +1,57 @@
 // Tests Go Here!
+describe("GameOfLife", function() {
+  describe("initialize", function() {
+    it("returns an array whose length = sizeX * sizeY", function() {
+      var gameOfLife = Object.create(GameOfLife);
+      gameOfLife.initialize(6, 6).length.should.equal(6*6);
+    });
+  });
+  describe("getIndex", function() {
+    it("returns the index into the game array when given x, y values", function() {
+      var gameOfLife = Object.create(GameOfLife);
+      gameOfLife.initialize(6, 6);
+      gameOfLife.getIndex(3, 3).should.equal(21);
+
+    });
+    it("returns -1 when given an x, y position outside of the world", function() {
+      var gameOfLife = Object.create(GameOfLife);
+      gameOfLife.initialize(10, 10);
+      gameOfLife.getIndex(-10, -10).should.equal(-1);
+      gameOfLife.getIndex(11, 12).should.equal(-1);
+    });
+  });
+  describe("makeLife", function() {
+    it("brings life to a given cell at x,y", function() {
+      var gameOfLife = Object.create(GameOfLife);
+      gameOfLife.initialize(100, 100);
+      gameOfLife.setStatus(2, 2, true);
+      gameOfLife.world[gameOfLife.getIndex(2, 2)].should.equal(true);
+
+    });
+  });
+  describe("getStatus", function() {
+    it("returns the status of a life cell at x, y", function() {
+      var gameOfLife = Object.create(GameOfLife);
+      gameOfLife.initialize(6, 6);
+      gameOfLife.setStatus(1, 1, true);
+      gameOfLife.setStatus(2, 2, false);
+
+      gameOfLife.getStatus(1, 1).should.equal(true);
+      gameOfLife.getStatus(2, 2).should.equal(false);
+      console.log(gameOfLife.world);
+    });
+  });
+  describe("countNeighbors", function() {
+    it("returns the number of neighbors for a cell at x, y", function() {
+      var gameOfLife = Object.create(GameOfLife);
+      gameOfLife.initialize(3,3);
+      gameOfLife.setStatus(2,0, true);
+      gameOfLife.setStatus(2,2, true);
+      gameOfLife.setStatus(0,2, true);
+      gameOfLife.countNeighbors(1,1).should.equal(3);
+
+    });
+  });
+
+});
+
